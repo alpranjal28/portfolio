0e5188ff12fc27336090c9171ba72021332a3597
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2024-05-10 18:58:50 +0530

    implementing google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
+const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_ID as string;
 
 export const metadata: Metadata = {
   title: "Portfolio | Pranjal",
@@ -19,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <TransitionProvider>{children}</TransitionProvider>
+        <GoogleAnalytics gaId={googleAnalyticsId} />
       </body>
     </html>
   );
