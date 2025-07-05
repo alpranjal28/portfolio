7533098a53cfdef6517541bd11472b0b927e9524
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2025-07-05 23:38:12 +0530

    added alternative to brain svg

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import Brain from "@/components/brain";
+import { Coffee } from "@/components/coffee";
+import { CoffeeCode } from "@/components/coffeeCode";
 import { skills } from "@/constants";
 import { motion, useInView, useScroll } from "framer-motion";
 import { useRef } from "react";
 
 const AboutPage = () => {
   const containerRef = useRef(null);
-  const { scrollYProgress } = useScroll({ container: containerRef });
+  // const { scrollYProgress } = useScroll({ container: containerRef });
 
   const skillRef = useRef(null);
   // const isSkillRefInView = useInView(skillRef, { once: true });
@@ -25,9 +27,9 @@ const AboutPage = () => {
       transition={{ duration: 0.5 }}
     >
       {/* CONTAINER */}
-      <div className=" h-full overflow-scroll lg:flex" ref={containerRef}>
+      <div className=" h-full overflow-scroll scrollbar-hide lg:flex" ref={containerRef}>
         {/* TEXT CONTAINER */}
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-24 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
           {/* BIOGRAPHY CONTAINER */}
           <div className="flex flex-col gap-12 justify-center">
             {/* BIOGRAPHY TITLE */}
@@ -254,7 +256,9 @@ const AboutPage = () => {
 
         {/* SVG CONTAINER */}
         <div className=" hidden lg:block w-1/3 xl:w-1/2 sticky top-0 z-30">
-          <Brain scrollYProgress={scrollYProgress}></Brain>
+          {/* <Brain scrollYProgress={scrollYProgress}></Brain> */}
+          {/* <Coffee/> */}
+          <CoffeeCode/>
         </div>
       </div>
     </motion.div>
